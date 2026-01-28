310011b9d9e1366d3f20927ad1f8ff582b254634
Mutahir Ahmed Khan <mutahirhere18@gmail.com>
2026-01-28 18:40:19 +0500

    Update and rename Question 01.ipynb to Question 01.py

---
+++ Lab-Tasks/Lab 01/Question 01.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+marks = int(input("Enter your marks: "))
+if marks >= 85:
+  print("Grade A")
+elif marks >= 70:
+  print("Grade B") 
+elif marks >= 50:
+  print("Grade C")
+else:
+  print("Fail")
+  
