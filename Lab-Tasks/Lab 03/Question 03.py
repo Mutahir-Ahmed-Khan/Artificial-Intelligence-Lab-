f82a18cf331f043ec39c611f03f1449844979344
Mutahir Ahmed Khan <mutahirhere18@gmail.com>
2026-02-11 22:14:09 +0500

    Create Question 03.py

---
+++ Lab-Tasks/Lab 03/Question 03.py
@@ -0,0 +1,9 @@
+print("Student Study Planner")
+print()
+
+courseList = ["AI", "Math", "Physics"]
+
+for course in courseList:
+    print(f"Studying {course}... Completed")
+
+print("Goal Achieved: All subjects completed")
